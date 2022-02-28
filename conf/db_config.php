ac6cac7dc70b5622d5c75de6a6c0183f85c10fe8
yalh76 <yalh@yahoo.com>
2022-02-28 03:36:19 +0100

    Implementation

---
+++ conf/db_config.php
@@ -0,0 +1,7 @@
+<?php
+define( 'DB_TYPE', 'mysql' );
+define( 'DB_NAME', '__DB_NAME__' );
+define( 'DB_USER', '__DB_USER__' );
+define( 'DB_PASSWORD', '__DB_PWD__' );
+define( 'DB_HOST', 'localhost' );
+?>
