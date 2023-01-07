cdc8d4bd188e8fa36b78c1f322cd20bb5744378e
Tagada <36127788+Tagadda@users.noreply.github.com>
2023-01-07 22:28:40 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
 # dependencies used by the app
 pkg_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql"
